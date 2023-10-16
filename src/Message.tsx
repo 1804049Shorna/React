f40b792cb6beac755cc995bcbc98c55b9809d078
Shanjida Nahid Shorna <sanjidanahid53@gmai.com>
2023-10-16 21:48:05 +0600

    Multiple Changes

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+
+//PascalCasing
+
+function Message(){
+
+    //JSX: Javascript XML
+    const name='Mosh';
+    return <h1>Hello {name}</h1>;
+}
+
+export default Message;
---
+++ src/componets/ListGroup.tsx
@@ -0,0 +1,35 @@
+
+
+function ListGroup() {
+let items=[
+        'new work',
+        'San fransisco',
+        'Tokyo'
+    ];
+//items=[];  
+// example of using the if conditions  
+// if(items.length==0)
+//      return <p>No item found</p>  
+
+// another way
+ // const message=items.length===0 ? <p>No item found</p> : null;
+
+  return (
+    
+    <>
+        <h1>This is a List</h1>
+        {items.length===0 && <p>No item found</p>}
+        <ul className="list-group">
+          {items.map((item,index)=>(
+          <li
+           className="list-group-item" 
+           key={item} 
+           onClick={(event)=> console.log(event)}
+           >
+            {item}</li>))}
+        </ul>
+    </>
+  );
+}
+
+export default ListGroup;
